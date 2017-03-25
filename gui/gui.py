a1bf85bf3389d46f0b9c260722fd33e3babaec05
Brian Stevens <tapemaster21@gmail.com>
2017-03-25 08:46:27 -0500

    Initial commit. Test UI made

---
+++ gui/gui.py
@@ -0,0 +1,15 @@
+from appJar import gui
+
+def press(name):
+	print("run script to get tweet")
+
+
+app = gui()
+app.addLabel("title","Mean Tweet finder", 0, 1)  # Row 0,Column 0,Span 2
+app.addLabel("tboxlabel", "Tweet URL:", 1, 0)
+app.addEntry("box", 1, 1)
+app.addButtons(["Analyze"], [press], 1, 2) # Row 3,Column 0,Span 2
+
+
+
+app.go()
