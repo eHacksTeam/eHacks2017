a0f6971f9ffc37fd1cd18035dd6af1c4728d2eed
Brian Stevens <tapemaster21@gmail.com>
2017-03-25 20:08:33 -0500

    GUI update. Added possible twit functions

--- gui.py
+++ gui.py
@@ -1,11 +1,28 @@
 from appJar import gui
 import classifier_lib as nn
+import twitter_bot as tb
+
+#tweetnumber = 10
+app = gui()
+#app2 = gui()
+
+# def updatelabel(name):
+# 	tweetnumber = int(app2.getEntry("numbox"))
+# 	print("this is where my for loop would go, IF I HAD ONE", tweetnumber)
+# 	app2.stop()
+
+# def stop(name):
+# 	app2.stop()
 
 
 def press(name):
 	url = app.getEntry("urlbox")
 	if url.lstrip()[0] is "@":
+		#user = get_user(url.lstrip()[0],tweetnum)
+		#get_tweets_from_user(user.name)
 		print("runscript for username")
+
+		# app2.go()
 		app.setLabel("problabel","Average user harrassment probility")
 	else:
 		print("run script to get tweet")
@@ -20,18 +37,28 @@ def prob(name):
 	app.setEntry("probbox",probability)
 
 
-app = gui()
-app.addLabel("title","Mean Tweet Analyzer",0,0,2) 
+def main():
 
-app.addEntry("urlbox", 1, 0)
-app.setEntry("urlbox","string input")
-app.addButtons(["Analyze"], [press], 1, 1, 2)
+	app.addLabel("title","Mean Tweet Analyzer",0,0,2) 
 
-app.addHorizontalSeparator(2,0,4)
+	app.addEntry("urlbox", 1, 0)
+	app.setEntry("urlbox","string input")
+	app.addButtons(["Analyze"], [press], 1, 1, 2)
 
-app.addLabel("problabel", "Probability", 3, 0, 2) 
-app.addEntry("probbox", 4, 0, 2) 
+	app.addHorizontalSeparator(2,0,4)
 
-#app.addButtons(["display indicators"], [press], 4, 0, 2)
+	app.addLabel("problabel", "Probability", 3, 0, 2) 
+	app.addEntry("probbox", 4, 0, 2) 
 
-app.go()
+
+
+	app2.addLabel("title","How many tweets do you want to assess",0,0,2) 
+	app2.addNumericEntry("numbox",10)
+	app2.addButtons(["Ok", "Cancel"], [updatelabel, stop])
+
+	#app.addButtons(["display indicators"], [press], 4, 0, 2)
+
+	app.go()
+
+if __name__ == "__main__":
+    main()
